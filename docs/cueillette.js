4272a4cc24155a9f68b6a70b25e632a9f5a36d61
Thomas Zilliox <tzilliox.externe@m6.fr>
2020-08-03 01:29:47 +0200

    Add debug to pick letter tool

--- docs/cueillette.js
+++ docs/cueillette.js
@@ -1,5 +1,6 @@
 (function formCueillette() {
     let output;
+    let debug;
 
     function update(formData) {
         let reference = formData.get('reference').trim();
@@ -20,12 +21,31 @@
             result += reference[index - 1];
         });
 
+        // Output
         if (output) {
             output.clear();
         }
         output = initOutput();
         output.setTitle('Résultat');
         output.setDetails(result);
+
+        // Debug
+        if (debug) {
+            debug.clear();
+        }
+        debug = initOutput();
+        debug.setTitle('En détails');
+        debug.setDetails(Array.from(reference).map((char, index) => {
+            let style = `font-family: monospace, monospace;`;
+            let title = '';
+            const pickIndex = pick.findIndex(value => value === (index + 1).toString());
+            if (pickIndex > -1) {
+                style += `background: #D8AE5C;`;
+                title = `Numéro ${index + 1} -> Position ${pickIndex + 1}`;
+            }
+
+            return `<span style="${style}" title="${title}">${char}</span>`;
+        }).join(''));
     }
 
     initForm('cueillette', update);
