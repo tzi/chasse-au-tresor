cd8186d3879bf69819d3cd3d2bf9ebe1111be65b
Thomas Zilliox <tzilliox.externe@m6.fr>
2020-08-06 00:06:17 +0200

    Add punctuation option, and more flexible picker input to cueillette tool

--- docs/cueillette.js
+++ docs/cueillette.js
@@ -7,19 +7,27 @@
         if (!formData.get('withSpaces')) {
             reference = reference.replace(/\s/g, '');
         }
+        if (!formData.get('withPunctuation')) {
+            reference = reference.replace(/[^a-zA-Z0-9\s]/g, '');
+        }
 
-        let result = '';
-        const pick = formData.get('pick').trim().replace(/\s+/g, ' ').split(' ');
-        pick.forEach(position => {
-            const index = parseInt(position, 10);
+        const pick = formData.get('pick')
+            .trim()
+            .replace(/\s+/g, ' ')
+            .match(/[\d]+|\D+/g);
+
+        const result = pick.map(item => {
+            const index = parseInt(item, 10);
             if (!index) {
-                return false;
+                return item;
             }
             if (!reference[index - 1]) {
+                console.error(`${index} not available in reference string`);
                 return false;
             }
-            result += reference[index - 1];
-        });
+
+            return reference[index - 1];
+        }).filter(item => item !== false).join('');
 
         // Output
         if (output) {
@@ -35,17 +43,21 @@
         }
         debug = initOutput();
         debug.setTitle('En détails');
-        debug.setDetails(Array.from(reference).map((char, index) => {
-            let style = `font-family: monospace, monospace;`;
-            let title = '';
-            const pickIndex = pick.findIndex(value => value === (index + 1).toString());
-            if (pickIndex > -1) {
-                style += `background: #D8AE5C;`;
-                title = `Numéro ${index + 1} -> Position ${pickIndex + 1}`;
-            }
-
-            return `<span style="${style}" title="${title}">${char}</span>`;
-        }).join(''));
+        debug.setDetails(`
+            <div style="word-wrap: break-word;">
+                ${Array.from(reference).map((char, index) => {
+                    let style = `font-family: monospace, monospace;`;
+                    let title = '';
+                    const pickIndex = pick.findIndex(value => value === (index + 1).toString());
+                    if (pickIndex > -1) {
+                        style += `background: #D8AE5C;`;
+                        title = `Numéro ${index + 1} -> Position ${pickIndex + 1}`;
+                    }
+        
+                    return `<span style="${style}" title="${title}">${char}</span>`;
+                }).join('')}
+            </div>
+        `);
     }
 
     initForm('cueillette', update);
