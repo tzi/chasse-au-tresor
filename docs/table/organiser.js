507516ee31be8a0f49f70bee30877123da747de9
THOMAS ZILLIOX <tzilliox.externe@bedrockstreaming.com>
2024-09-19 23:14:01 +0200

    Allow different starting point and different directions

--- docs/table/organiser.js
+++ docs/table/organiser.js
@@ -16,12 +16,22 @@
         }).join('');
         const width = getInput(formData, 'width', { isNumber: true });
         const height = getInput(formData, 'height', { isNumber: true });
+        const startPoint = formData.get('start');
+        const direction = formData.get('direction');
+        const getIndex = function(rowIndex, colIndex) {
+            const relativeColIndex = startPoint.includes('r') ? width - 1 - colIndex : colIndex;
+            const relativeRowIndex = startPoint.includes('b') ? height - 1 - rowIndex : rowIndex;
+            const [x, y] = direction === 'hv' ? [relativeColIndex, relativeRowIndex] : [relativeRowIndex, relativeColIndex];
+            const stepSize = direction === 'hv' ? width : height;
+
+            return y * stepSize + x;
+        }
 
         let html = '<table style="border-collapse: collapse; width: 100%;">';
         for (let rowIndex = 0; rowIndex < height; rowIndex++) {
             html += '<tr>';
             for (let colIndex = 0; colIndex < width; colIndex++) {
-                const index = rowIndex * width + colIndex;
+                const index = getIndex(rowIndex, colIndex);
                 html += '<td style="border: 1px solid black; text-align: center;">' + (referenceList[index] || '') + '</td>';
             }
             html += '</tr>';
